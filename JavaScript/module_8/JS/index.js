8b5ca056f25a8d66ab0971c5be02de89d6f0b5ba
Umberban <romanovrodionv@gmail.com>
2019-03-19 10:02:00 +0200

    module 8

---
+++ JavaScript/module_8/JS/index.js
@@ -0,0 +1,37 @@
+const galleryItems = [
+  { preview: './Img/FisrtSmall.jpg', fullview: './Img/FisrtLarge.jpg', alt: "alt text 1" },
+  { preview: './Img/SecondSmall.jpg', fullview: './Img/SecondLarge.jpg', alt: "alt text 2" },
+  { preview: './Img/ThirdSmall.jpg', fullview: './Img/ThirdLarge.jpg', alt: "alt text 3" },
+  { preview: './Img/FiveSmall.jpg', fullview: './Img/FiveLarge.jpg', alt: "alt text 4" },
+  { preview: './Img/SixSmall.jpg', fullview: './Img/SixLarge.jpg', alt: "alt text 5" },
+];
+
+const box = document.querySelector('.js-image-gallery');
+
+window.addEventListener("DOMContentLoaded", function(){
+  const full = document.createElement('div');
+  full.classList.add('fullview');
+  let fullImg = `<img class='big' style= 'width:100%' src=${galleryItems[0].preview} data-fullview = ${galleryItems[0].fullview} alt=${galleryItems[0].alt}>`;
+  full.innerHTML= fullImg;
+  box.appendChild(full);
+  full.style.textAlign ="center";
+const preview = document.createElement('ul');
+preview.classList.add('preview');
+preview.style.display = 'flex';
+preview.style.listStyle = 'none';
+box.style.width = "700px";
+box.style.margin = "auto";
+
+
+
+
+let previewList = galleryItems.reduce((acc,i) => acc + `<li><img style= 'width:100% height = 100px 'src=${i.preview} data-fullview = ${i.fullview} alt=${i.alt}> </li>`,"");
+preview.innerHTML= previewList;
+box.appendChild(preview);
+const bigImg = document.querySelector('.big');
+
+box.addEventListener('click', function(e){
+  if(e.target.nodeName !== "IMG") return;
+  bigImg.src = e.target.dataset.fullview;
+});
+});
